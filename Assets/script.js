ebb48401e1983788c67f07f841eba4a4078d9916
Sheryl L <s_lawrence0@yahoo.ca>
2024-01-29 17:45:25 -0500

    js code added

--- Assets/script.js
+++ Assets/script.js
@@ -1,8 +1,8 @@
 var startButton = document.querySelector(".start-button");
-let questionsEl = 
-    document.querySelector( 
-        "#questions"
-    );
+var timerElement = document.querySelector(".timer");
+
+var questionsEl = document.querySelector("#questions");
+var submitButton = document.querySelector(".submit-score-button");
 
 prompt: 'How do we assign a value to a variable?',
 options: [   
@@ -12,3 +12,37 @@ options: [
     "4. @",
 ],
 answer: "myFunction()",
+
+/*  Question 2: What do primitive data types not include from the options available?
+    A) Undefined
+    B) String
+    C) Number 
+    D) True / Typeof
+
+    Question 3: What can be used to store groups of data in a single variable?
+    A) Arrays
+    B) Iterations
+    C) Methods
+    D) Scope
+
+    Question 4: Properties are made up of key-value pairs. What are objects a collection of?
+    A) Variables
+    B) Strings
+    C) Properties / Key-value pairs 
+    D) Console logs
+
+    Question 5: What do we use/console.log to access a value stored in a variable?
+    A) Typeof
+    B) Value
+    C) Variable's name
+    D) Length
+*/
+
+var time = questions.length * 60;
+
+functions startQuiz () {
+    time = setInterval (
+        clockTick,
+        1000
+    );
+}
