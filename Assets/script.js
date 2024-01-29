789c638d89ff4a26f1cb1d3fec52a5c8baf56257
Sheryl L <s_lawrence0@yahoo.ca>
2024-01-29 17:29:31 -0500

    css styling and html changes and started js

---
+++ Assets/script.js
@@ -0,0 +1,14 @@
+var startButton = document.querySelector(".start-button");
+let questionsEl = 
+    document.querySelector( 
+        "#questions"
+    );
+
+prompt: 'How do we assign a value to a variable?',
+options: [   
+    "1. +",
+    "2. =",
+    "3. %",
+    "4. @",
+],
+answer: "myFunction()",
