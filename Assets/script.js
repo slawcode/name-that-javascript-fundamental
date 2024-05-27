f0d1a1ac5a36f27da7c9fba84e3f28e3a1d8dee9
Sheryl L <s_lawrence0@yahoo.ca>
2024-05-27 14:44:44 -0400

    highscore.html file added, edits to index.html and startGame function added to script.js

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,5 @@
-var startButton = document.querySelector(".start-button");
+// DOM elements
+var startButton = document.querySelector(".startButton");
 var timerElement = document.querySelector(".timer");
 
 var questionsEl = document.querySelector("#questions");
@@ -45,27 +46,45 @@ var questions = [
 
 var time = questions.length * 60;
 
-function startQuiz () {
-    time = setInterval (
-        clockTick,
-        1000
-    );
+// function startQuiz () {
+//     time = setInterval (
+//         clockTick,
+//         1000
+//     );
+// }
+
+function startGame() {
+    isWin = false;
+    timerCount = 10;
+    // Prevents start button from being clicked when round is in progress
+    startButton.disabled = true;
+    renderBlanks()
+    startTimer()
 }
-function get Question () {
-    var currentQuestion =
-    questions[currentQuestionIndex];
-    var promptEL =
-    document.getElementbyClass (
-        "question-words"
-    );
-    promptEl.textContent = 
-        currentQuestion.prompt; 
-    choicesEl.innerHTML = ""; 
-    currentQuestion.options.forEach( 
-        function (choice, i) { 
-            let choiceBtn = 
-                document.createElement( 
-                    "button"
-                ); 
-}
-// (Create a Quiz App with Timer, 2023).
+
+// Start the quiz when the 'Start Quiz' button is clicked
+// startBtn.onclick = startQuiz;
+// Attach event listener to start button to call startGame function on click
+startButton.addEventListener("click", startGame);
+
+// Calls init() so that it fires when page opened
+init();
+
+// function getQuestion () {
+//    var currentQuestion = questions[currentQuestionIndex];
+//     
+//    var promptEL =
+//    document.getElementbyClass ("question-words");
+//    promptEl.textContent = 
+//    currentQuestion.prompt; 
+//    choicesEl.innerHTML = ""; 
+//    currentQuestion.options.forEach( 
+//    function (choice, i) { 
+//    let choiceBtn = 
+//    document.createElement("button"); 
+// }
+
+
+
+
+// Create a Quiz App with Timer, 2023
